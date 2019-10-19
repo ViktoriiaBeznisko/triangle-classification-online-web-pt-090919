5e6a0237e19589760dc5a00933ba8b6ce34498db
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-19 01:25:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,14 +9,18 @@ class Triangle
   end
 
   def kind
+
     if @x == @y && @y == @z
       :equilateral
     elsif @x == @y && @x != @z || @x == @z && @x != @y || @y == @z && @y != @x
       :isosceles
     elsif @x != @y && @y != @z
       :scalene
-    elsif x == 0 || y == 0 || z == 0
+    else
+      if @x == 0 || @y == 0 || @z == 0
         raise TriangleError
+
+
 
 
   end
