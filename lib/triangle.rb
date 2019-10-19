2faf7311b22a03df926ee7684481330b16729e8d
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-19 01:23:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
       :isosceles
     elsif @x != @y && @y != @z
       :scalene
-    elsif @x == 0 || @y == 0 || @z == 0
+    elsif x == 0 || y == 0 || z == 0
         raise TriangleError
 
 
