ffec399014f5f04004cac4126943f9cfdd82b471
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-19 00:32:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
   # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(attributes)
+     attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+
+class TriangleError < StandardError
+    # triangle error code
+end
+
 end
